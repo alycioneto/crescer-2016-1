d59af1c9f33859f20d2c49967a929fedf256263e
alycioneto <alycio@gmail.com>
2016-04-06 10:50:11 -0300

    refactor(DividirEmPares)Retirar o desnecessário.

--- src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/CestoDeLembas.java
@@ -1,16 +1,10 @@
 public class CestoDeLembas{
     private int lembas;
-    private int personagens;
-    public CestoDeLembas(int lembas,int personagens){
+    public CestoDeLembas(int lembas){
         this.lembas = lembas;
-        this.personagens = personagens;
     }
     public boolean podeDividirEmPares(){
-        if(personagens >= 1 && personagens <= 100){
-            if (lembas % personagens == 0 && lembas != personagens && lembas % 2 == 0)
-                return true;
-        }  
-     return false;
-    }
+            return (lembas >= 1 && lembas <= 100) && (lembas != 2 && lembas % 2 == 0);
 
+    }
 }
