dd472fd213ac76b514e92380375bc0421cf37764
alycioneto <alycio@gmail.com>
2016-04-06 01:37:17 -0300

    feat(classe)Criação classe Dwarves

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarves.java
@@ -0,0 +1,9 @@
+public class Dwarves{
+    private int vida = 110;
+    public int getVida(){
+        return this.vida;
+    }
+    public void setVida(int vida){
+        this.vida = vida;
+    }
+}
