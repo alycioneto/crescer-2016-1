964ccb3f1050cf93f858a1a0201769cc4fcc150e
alycioneto <alycio@gmail.com>
2016-04-06 01:35:38 -0300

    feat(adicionar)adicionar atirarFlechaDwarves

--- src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Elfo.java
@@ -1,13 +1,25 @@
-public class Elfo {
+public class Elfo{
     private String nome;
-    private int experiencia, flechas = 42;
-    
-    public Elfo(String nome) {
+    private int flechas;
+    private int experiencia = 0;
+    public Elfo(String nome){
         this.nome = nome;
+        this.flechas = 42;
     }
-    
-    public void atirarFlecha() {
-        experiencia++;
-        flechas--;
+    public Elfo(String nome, int flechas){
+        this.nome = nome;
+        this.flechas = flechas;
+    }
+    public void atirarFlecha(){
+        this.flechas--;
+        this.experiencia++;
+        
+        
+    }
+    public void atirarFlechaDwarves(Dwarves dwarf){
+        if(dwarf.getVida() > 0){
+            atirarFlecha();
+            dwarf.setVida(dwarf.getVida() - 10);
+        }
     }
 }
