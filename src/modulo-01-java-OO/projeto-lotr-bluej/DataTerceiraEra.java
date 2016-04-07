d9ff9cf31e8f32236f30e8a072b9801687700c58
alycioneto <alycio@gmail.com>
2016-04-06 21:12:41 -0300

    feat(DataTerceiraEra) Implementação de classe

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DataTerceiraEra.java
@@ -0,0 +1,20 @@
+public class DataTerceiraEra{
+    private int dia,mes,ano;
+    public DataTerceiraEra(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+    public int getDia(){
+        return this.dia;
+    }
+    public int getMes(){
+        return this.mes;
+    }
+    public int getAno(){
+        return this.ano;
+    }
+    public boolean ehBissexto(){
+        return  ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0);
+    }
+}
