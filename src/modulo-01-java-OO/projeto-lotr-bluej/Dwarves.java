7cc34cb5f3c53651cc908b61c60ec20142eab710
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-07 16:18:26 -0300

    feat(Status) Implementar status dos Dwarves  e getStatus.

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarves.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarves.java
@@ -1,12 +1,16 @@
 public class Dwarves{
     private int vida;
     private String nome;
+    private Status status = Status.VIVO;
     public Dwarves(String nome){
         this.vida = 110;
         this.nome = nome;
     }
     public void perdeVida(){
-        this.vida -= 10;
+         if (vida >= 10) 
+            this.vida -= 10;
+            if (vida == 0)
+                status = Status.MORTO;
     }
     public void setNome(String nome){
         this.nome = nome;
@@ -17,5 +21,8 @@ public class Dwarves{
     public int getVida(){
         return this.vida;
     }
-   
+    public Status getStatus(){
+        return this.status;
+    }
+  
 }
