e6175ac28613c5e16dccc123a33f0f113075e11b
alycioneto <alycio@gmail.com>
2016-04-08 01:37:19 -0300

    feat(InventarioTest) Colocando e tirando itens

--- src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -5,10 +5,21 @@ import org.junit.Test;
 public class InventarioTest
 {
     @Test
-    public void verificarItemAdicionar(){
+    public void colocandoItemNoIventario(){
         Item espada = new Item(1, "Espada");
         Inventario bolsa = new Inventario();
         bolsa.adicionarItem(espada);
-        assertEquals(bolsa(0).getDescricao(),espada.getDescricao());
+        assertTrue(bolsa.getLista().size() == 1);
     }
+    @Test
+    public void tirandoItemDoIventario(){
+        Item espada = new Item(1, "Espada");
+        Inventario bolsa = new Inventario();
+        bolsa.adicionarItem(espada);
+        assertTrue(bolsa.getLista().size() == 1);
+        bolsa.removerItem(espada);
+        assertTrue(bolsa.getLista().size()== 0);
+    }
+   
 }
+
