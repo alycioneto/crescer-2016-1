02699a93b829b11fe91696633f010489e46b46bc
alycioneto <alycio@gmail.com>
2016-04-08 01:42:52 -0300

    feat(Dwarves)Implmentado getNumSorte
    alterado o método perdeVida, adicionado atributos vida e experciencia, os metodos getvida e getExperiencia

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarves.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarves.java
@@ -1,16 +1,17 @@
 public class Dwarves{
-    private int vida;
+    private int vida, experiencia = 0;;
     private String nome;
     private Status status = Status.VIVO;
+    private Inventario inventario = new Inventario();
+    private DataTerceiraEra dataNascimento;
     public Dwarves(String nome){
         this.vida = 110;
         this.nome = nome;
+        this.dataNascimento = new DataTerceiraEra(1,1,1);
     }
-    public void perdeVida(){
-         if (vida >= 10) 
-            this.vida -= 10;
-            if (vida == 0)
-                status = Status.MORTO;
+    public Dwarves(String nome, DataTerceiraEra dataNascimento){
+        this(nome);
+        this.dataNascimento = dataNascimento;
     }
     public void setNome(String nome){
         this.nome = nome;
@@ -18,11 +19,49 @@ public class Dwarves{
     public String getNome(){
         return this.nome;
     }
+    public int getExperiencia(){
+        return this.experiencia;
+    }
     public int getVida(){
         return this.vida;
     }
     public Status getStatus(){
         return this.status;
+    }
+    public Inventario getInventario(){
+        return this.inventario;
+    }
+    public DataTerceiraEra getDataNacimento(){
+        return this.dataNascimento;
+    }
+    public void perdeVida(){
+        if(getNumeroSorte() < 0)
+            experiencia = experiencia + 2;
+        else if(getNumeroSorte() > 100){
+            int vidaAposFlechada = this.vida -10;
+            if (vidaAposFlechada == 0) 
+               status = Status.MORTO;
+            if (vida > 0)
+               vida =  vidaAposFlechada;        
+        }
+    }
+    public void adicionarItem(Item item){
+        this.inventario.adicionarItem(item);
+    }
+    public void perderItem(Item item){
+        this.inventario.removerItem(item);
+    }
+    public double getNumeroSorte(){
+        double numeroDaSorte = 101.0;
+        if (dataNascimento.ehBissexto() == true && (vida >= 80 && vida <= 90)){
+            numeroDaSorte = numeroDaSorte * -33;
+            return numeroDaSorte;
+        }
+        else if (dataNascimento.ehBissexto() == false && (nome == "Seixas" || nome == "Meireles")){
+            numeroDaSorte = (numeroDaSorte * 33) % 100;
+            return numeroDaSorte;
+        }
+        return numeroDaSorte;
     }
   
 }
