aff32ef989b342a9607047072203b11faff57479
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-08 16:54:06 -0300

    feat(inventario) Implementado metodo Imprimir

--- src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Inventario.java
@@ -1,10 +1,21 @@
 import java.util.ArrayList;
 public class Inventario{
-    ArrayList<Item> lista = new ArrayList<>();
+    private ArrayList<Item> lista = new ArrayList<>();
     public void adicionarItem(Item item){
         this.lista.add(item);
     }
     public void removerItem(Item item){
            this.lista.remove(item);
     }
+    public ArrayList<Item> getLista(){
+        return this.lista;
+    }
+    public String imprimir(){
+        String itens = "";
+        for(int i =0; i < lista.size(); i++){
+                itens += lista.get(i).getDescricao() + ",";   
+        }
+        return !itens.isEmpty() ? itens.substring(0, itens.length() -1) : itens;
+    }
+    
 }
