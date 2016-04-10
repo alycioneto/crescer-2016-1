97ef03ab8c64e73d77f7bee40b99ae420a0c1e32
alycioneto <alycio@gmail.com>
2016-04-10 01:57:35 -0300

    feat(HobbitContador)Implementada classe
    HobbitContador

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class HobbitContador
+{
+   public int contador(ArrayList<ArrayList<Integer>> arrayDePares){
+       int somaDifDoProdEMMC =0;
+       ArrayList<Integer> diferencaDoProdutoEMMC = new ArrayList<>();
+       for (int i = 1; i <= arrayDePares.size(); i++){
+           int auxI = i -1;
+           for(int j = 1 ; j < arrayDePares.get(auxI).size();j++){
+               int auxJ = j-1;
+                   int produto = arrayDePares.get(auxI).get(auxJ) * arrayDePares.get(auxI).get(auxJ+1);
+                   int mmc = minimoMultiploComum(arrayDePares.get(auxI).get(auxJ),arrayDePares.get(auxI).get(auxJ +1));
+                   diferencaDoProdutoEMMC.add(produto-mmc);
+           }
+           somaDifDoProdEMMC = somaDifDoProdEMMC + diferencaDoProdutoEMMC.get(auxI);
+       }
+       return somaDifDoProdEMMC;    
+   }
+   public int minimoMultiploComum(int numA, int numB){
+       int multNumA = 1,multNumB = 1;
+        for (int n = 1;; n++) {
+            multNumA = numA * n;
+            for (int x = 1; multNumB < multNumA; x++) {
+                multNumB = numB * x;
+            }
+            if (multNumA == multNumB)
+                break;
+        }
+        return multNumA;
+    } 
+}  
+
+
