7269229bae294b6d89c8f91358dc11c66ac7b7fc
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-11 16:51:29 -0300

    fix(setNome) Metodo não utilizado

--- src/modulo-01-java-OO/projeto-lotr-bluej/SerVivo.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/SerVivo.java
@@ -5,9 +5,6 @@ public class SerVivo{
     public SerVivo(String nome){
         this.nome = nome;
     }
-    public void setNome(String nome){
-        this.nome = nome;
-    }
     public String getNome(){
         return this.nome;
     }
