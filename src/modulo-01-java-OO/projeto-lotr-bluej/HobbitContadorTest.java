9c8b1f3a1219b060bfc118fb822cc2ea43d4c62a
alycioneto <alycio@gmail.com>
2016-04-11 00:49:14 -0300

    fix(calculandoADiferencaCorretamente)
    Chamada errada, devida alteração do nome de um metodo na classe HobbitContador.

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContadorTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class HobbitContadorTest
 {
     @Test
-    public void contadorRetornandoCorretamente(){
+    public void calculandoADiferencaCorretamente(){
        HobbitContador frodo = new HobbitContador();
        ArrayList<ArrayList<Integer>> arrayDePares = new ArrayList<>();
        arrayDePares.add(new ArrayList<>(Arrays.asList(15, 18)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(4, 5)));
        arrayDePares.add(new ArrayList<>(Arrays.asList(12, 60)));
-       assertEquals(frodo.contador(arrayDePares),840);
+       assertEquals(frodo.calcularDiferenca(arrayDePares),840);
     }
     @Test
     public void verificarOMMC(){
