3d4935a15561a6d6b5c9f3b0f9348f4f2436e136
alycioneto <alycio@gmail.com>
2016-04-11 00:46:53 -0300

    refact(calcularDiferenca) Laco desnecessário, nome
    Incorreto

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -1,16 +1,13 @@
 import java.util.*;
 public class HobbitContador
 {
-   public int contador(ArrayList<ArrayList<Integer>> arrayDePares){
+   public int calcularDiferenca(ArrayList<ArrayList<Integer>> arrayDePares){
        int somaDifDoProdEMMC =0;
        for (int i = 1; i <= arrayDePares.size(); i++){
            int auxI = i -1;
-           for(int j = 1 ; j < arrayDePares.get(auxI).size();j++){
-               int auxJ = j-1;
-                   int produto = arrayDePares.get(auxI).get(auxJ) * arrayDePares.get(auxI).get(auxJ+1);
-                   int mmc = minimoMultiploComum(arrayDePares.get(auxI).get(auxJ),arrayDePares.get(auxI).get(auxJ +1));
-                   somaDifDoProdEMMC = somaDifDoProdEMMC + (produto-mmc);
-           }
+           int produto = arrayDePares.get(auxI).get(0) * arrayDePares.get(auxI).get(1);
+           int mmc = minimoMultiploComum(arrayDePares.get(auxI).get(0),arrayDePares.get(auxI).get(1));
+           somaDifDoProdEMMC = somaDifDoProdEMMC + (produto-mmc);
        }
        return somaDifDoProdEMMC;    
    }
