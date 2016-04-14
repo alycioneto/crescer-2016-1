827c1f76042a29d787c9cd9561b88e5be2cc113f
alycioneto <alycio@gmail.com>
2016-04-14 01:54:49 -0300

    Test(encontrarElfosVivos) Verificando se o
    retorno dos elfos vivos esta correto.

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfosTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
 public class ExercitoDeElfosTest
 {
     @Test
@@ -25,6 +26,26 @@ public class ExercitoDeElfosTest
         exercito.alistarElfos(e2);
         Elfo obtido = exercito.getElfoDoExercitoPeloNome("Legolas");
         assertEquals(legolas , obtido);
+    }
+    @Test
+    public void encontrarElfosVivos(){
+        Elfo e0 = new ElfoNoturno("e0");
+        Elfo e1 = new ElfoNoturno("e1");
+        Elfo e2 = new ElfoNoturno("e2");
+        Elfo e3 = new ElfoNoturno("e3");
+        ExercitoDeElfos exercito = new ExercitoDeElfos();
+        exercito.alistarElfos(e0);
+        exercito.alistarElfos(e1);
+        exercito.alistarElfos(e2);
+        exercito.alistarElfos(e3);
+        exercito.agruparPorStatus();
+        ArrayList<Elfo> elfosEsperados = new ArrayList<>();
+        elfosEsperados.add(e0);
+        elfosEsperados.add(e1);
+        elfosEsperados.add(e2);
+        elfosEsperados.add(e3);
+        ArrayList<Elfo> elfosObtidos = exercito.buscar(Status.VIVO);
+        assertTrue(elfosEsperados.equals(elfosEsperados));
     }
     
 }
