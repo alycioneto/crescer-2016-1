f5c245f6579d2b8f576e46a344a3f99d7bd70a59
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-15 14:49:02 -0300

    Revert (ArteDaGuerra) Remoção de linha repetida

--- src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ArteDaGuerra.java
@@ -5,7 +5,6 @@ public class ArteDaGuerra implements EstrategiaDeAtaque{
         return this.ordemDoUltimoAtaque;
     }
     public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarves> dwarves){
-        double elfosNoturnosPodemAtacar =  ((int)((pelotao.size() * dwarves.size()) * 0.30));
         int elfosNoturnosPodemAtacar =  ((int)((pelotao.size() * dwarves.size()) * 0.30));
         int ataquesElfosNoturnos = 0;
         ordemDoUltimoAtaque = new ArrayList<>(); 
