d55db4ea7593f011670e2a357a2f48a31e251621
alycioneto <alycio@gmail.com>
2016-04-15 04:51:26 -0300

    fear(EstrategiaDeAtaque) implementada a interface

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaDeAtaque.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+public interface EstrategiaDeAtaque {
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque();
+    public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarves> dwarves);
+} 
