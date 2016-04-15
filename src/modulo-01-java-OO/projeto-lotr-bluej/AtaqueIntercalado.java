55d9a75f4863e73b0f72a073f36c0f93fbd3d4cf
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-15 15:42:39 -0300

    Feat(AtaqueIntercalado) validando se a quantidade de elfos verdes e Notunos sao iguais

--- src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercalado.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/AtaqueIntercalado.java
@@ -8,27 +8,37 @@ public class AtaqueIntercalado implements EstrategiaDeAtaque
    public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarves> dwarves){
        ordemDoUltimoAtaque = new ArrayList<>();
        String tipoDoUltimoAtaque = "";
-       for(int i = 0; i <= pelotao.size() -1; i++){
-           if(ordemDoUltimoAtaque.size()== 0 ){
-               for(Dwarves dwarf : dwarves){
-                   pelotao.get(i).atirarFlecha(dwarf);
-                   tipoDoUltimoAtaque = pelotao.get(i).getClass().getName();
-               }
-               this.ordemDoUltimoAtaque.add(pelotao.get(i));
-           }
-           else if (tipoDoUltimoAtaque == pelotao.get(i).getClass().getName()){
-               Elfo voltaProFimDaFila = pelotao.get(i);
-               pelotao.remove(i);
-               pelotao.add(voltaProFimDaFila);
-               i--;
-           } 
-           else {
-              for(Dwarves dwarf : dwarves){
-                  pelotao.get(i).atirarFlecha(dwarf);
-                  tipoDoUltimoAtaque = pelotao.get(i).getClass().getName();
-              }
-              this.ordemDoUltimoAtaque.add(pelotao.get(i));
-           }
+       int elfosNoturnos =0;
+       int elfosVerdes =0;
+       for(Elfo elfo: pelotao){
+           if(elfo instanceof ElfoVerde)
+                elfosVerdes++;
+           else
+                elfosNoturnos++;
        }
-   }
+       if (elfosNoturnos == elfosVerdes){
+           for(int i = 0; i <= pelotao.size() -1; i++){
+               if(ordemDoUltimoAtaque.size()== 0 ){
+                   for(Dwarves dwarf : dwarves){
+                       pelotao.get(i).atirarFlecha(dwarf);
+                       tipoDoUltimoAtaque = pelotao.get(i).getClass().getName();
+                    }
+                    this.ordemDoUltimoAtaque.add(pelotao.get(i));
+                }
+                else if (tipoDoUltimoAtaque == pelotao.get(i).getClass().getName()){
+                    Elfo voltaProFimDaFila = pelotao.get(i);
+                    pelotao.remove(i);
+                    pelotao.add(voltaProFimDaFila);
+                    i--;
+                } 
+                else {
+                    for(Dwarves dwarf : dwarves){
+                        pelotao.get(i).atirarFlecha(dwarf);
+                        tipoDoUltimoAtaque = pelotao.get(i).getClass().getName();
+                    }
+                    this.ordemDoUltimoAtaque.add(pelotao.get(i));
+                }
+            }
+        }
+    }
 }
