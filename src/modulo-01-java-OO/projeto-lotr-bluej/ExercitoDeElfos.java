63941fc54f4e462003d306c14ee3027b4bb807d3
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-04-15 15:03:32 -0300

    feat(ExercitoDe Elfos) Implementado metodos Atacar, getOrdemDoUltimoAtaque e mudarEstrategia

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
@@ -1,29 +1,41 @@
 import java.util.*;
 public class ExercitoDeElfos{
-     private HashMap<String,Elfo> exercito= new HashMap<>();
-     private HashMap<Status,ArrayList<Elfo>> statusDoExercito = new HashMap<>();
-     public void alistarElfos(Elfo elfo){
+    private HashMap<String,Elfo> exercito= new HashMap<>();
+    private HashMap<Status,ArrayList<Elfo>> statusDoExercito = new HashMap<>();
+    private EstrategiaDeAtaque estrategia = new NoturnosPorUltimo();
+    public void alistarElfos(Elfo elfo){
          if(elfo instanceof ElfoNoturno || elfo instanceof ElfoVerde){
              exercito.put(elfo.getNome(),elfo);
          }   
-     }
-     public Elfo getElfoDoExercitoPeloNome(String nome){
+    }
+    public Elfo getElfoDoExercitoPeloNome(String nome){
          Elfo elfo = exercito.get(nome);
          return elfo;
-     }
-     public HashMap<String,Elfo> getExercito(){
+    }
+    public HashMap<String,Elfo> getExercito(){
          return this.exercito;
-     }
+    }
      //http://stackoverflow.com/questions/12134687/how-to-add-element-into-arraylist-in-hashmap
-     public void agruparPorStatus(){
-        statusDoExercito.clear();
-        for(Elfo elfo : exercito.values()){
+    public void agruparPorStatus(){
+       statusDoExercito.clear();
+       for(Elfo elfo : exercito.values()){
             if(!statusDoExercito.containsKey(elfo.getStatus()))
                 statusDoExercito.put(elfo.getStatus(), new ArrayList<Elfo>());
             statusDoExercito.get(elfo.getStatus()).add(elfo);
-        }
-     }
-     public ArrayList<Elfo> buscar(Status status){
+       }
+    }
+    public ArrayList<Elfo> buscar(Status status){
          return statusDoExercito.get(status);
-     }
+    }
+      public void atacar(ArrayList<Dwarves> alvos) {
+          this.estrategia.atacar(buscar(Status.VIVO), alvos);
+    }
+
+    public ArrayList<Elfo> getOrdemDoUltimoAtaque() {
+        return this.estrategia.getOrdemDoUltimoAtaque();
+    }
+
+    public void mudarEstrategia(EstrategiaDeAtaque estrategia) {
+        this.estrategia = estrategia;
+    }
 }
