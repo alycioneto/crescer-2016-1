290cdf64bd94072255a4ff8f9390f7b45d9c8d6c
alycioneto <alycio@gmail.com>
2016-04-15 04:54:13 -0300

    feat(NoturnosPorUltimo) implementada estrategia

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/NoturnosPorUltimo.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class NoturnosPorUltimo implements EstrategiaDeAtaque
+{
+   private ArrayList<Elfo> ordemDoUltimoAtaque;
+   public ArrayList<Elfo> getOrdemDoUltimoAtaque(){
+        return this.ordemDoUltimoAtaque;
+   }
+   public void atacar(ArrayList<Elfo> pelotao, ArrayList<Dwarves> dwarves){
+       ordemDoUltimoAtaque = new ArrayList<>();
+       ArrayList<Elfo> elfosNoturnos = new ArrayList<>();
+       for(Elfo elfo : pelotao){
+           if (elfo instanceof ElfoVerde){
+               for (Dwarves dwarf : dwarves){
+                   elfo.atirarFlecha(dwarf);
+                   this.ordemDoUltimoAtaque.add(elfo);
+               }
+           }
+           else
+                elfosNoturnos.add(elfo);
+       }     
+       for(Elfo elfo : elfosNoturnos){
+           for (Dwarves dwarf : dwarves){
+               elfo.atirarFlecha(dwarf);
+               this.ordemDoUltimoAtaque.add(elfo);
+           }
+       }
+   }
+}
