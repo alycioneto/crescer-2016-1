5c4458cd934a605fd88c8106b12c3dc133dcd7c2
alycioneto <alycio@gmail.com>
2016-04-21 16:30:58 -0300

    feat(exercicio2) Ainda falta terminar a
    10, 11 , 14

--- src/modulo-02-banco/exercicio2.sql
+++ src/modulo-02-banco/exercicio2.sql
@@ -7,8 +7,41 @@ select nomeEmpregado, DATEDIFF(MONTH, dataadmissao, CONVERT(date, '31/12/2000',1
 	from empregado
 	where dataadmissao between CONVERT(date, '01/05/1980',103) and CONVERT(date ,'20/01/1982',103)
 --4
-select top 1 Cargo,
-	COUNT(1) as Total
-	From Empregado
-	group by Cargo
-	order by Total desc
+select top 1 Cargo,	COUNT(1) as Total From Empregado group by Cargo	order by Total desc
+--6
+select nome, DATEADD(year, 50, datanascimento) as [Completa 50 anos], DATENAME(dw,DataNascimento) from Associado;
+--7
+select UF, COUNT(1) as Estado from Cidade group by UF order by Estado desc;
+--8
+-- sql not distinct
+--http://stackoverflow.com/questions/13146304/how-to-select-every-row-where-column-value-is-not-distinct
+select distinct nome, uf from cidade group by nome, uf having count(1) > 1;
+--9
+select top 1 (idassociado + 1) from associado group by idassociado order by idassociado desc;
+--10 *************
+--11 -- Falta Terminar
+begin transaction
+go
+update cidade set nome = '*' + nome where nome in() ;
+rollback
+
+--12
+select IDAssociado, nome, 
+	Case when sexo = 'F' then 'Feminino'
+		 when sexo = 'M' then 'Masculino'
+		 else 'sei lá'
+	End Genero
+from associado;
+
+--13
+select NomeEmpregado, salario, 
+		Case when salario < 1164 then '0%'
+			when salario between  1164 and 2326 then '15%'
+			else '27,5'
+		end impostoRenda
+from empregado;
+
+--15
+--http://www.w3schools.com/sql/sql_unique.asp
+alter table cidadeaux add constraint UK_Nome_UF UNIQUE (nome,UF)
+
