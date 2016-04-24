afbb9243f74869922e490579ba36eb1e50018022
alycioneto <alycio@gmail.com>
2016-04-23 22:03:14 -0300

    feat(Exercicio3) Falta terminar, feito até o 5

---
+++ src/modulo-02-banco/exercicio3.sql
@@ -0,0 +1,19 @@
+--1
+Select e.NomeEmpregado, d.NomeDepartamento from Empregado e inner join Departamento d on e.IDDepartamento = d.IDDepartamento;
+--2
+select a.nome, c.Nome from Associado a left join Cidade c on a.IDCidade = c.IDCidade
+--3
+select c.UF, COUNT(1) from Cidade c where UF not in (select c.UF from Associado a inner join Cidade c on a.IDCidade = c.IDCidade) group by UF;
+--4
+select a.Nome, c.nome ,
+	Case when c.UF in ('RS', 'SC' , 'PR') then '***'
+	else null
+	End [Estado_Sul]
+from Associado a inner join Cidade c on a.IDCidade = c.IDCidade;
+--5
+--http://stackoverflow.com/questions/14995000/sql-inner-join-more-than-two-tables
+
+Select e.NomeEmpregado, d.NomeDepartamento, g.NomeEmpregado as NomeGerente
+from Empregado e inner join Departamento d on e.IDDepartamento = d.IDDepartamento
+inner join Empregado g on e.IDGerente = g.IDEmpregado
+
