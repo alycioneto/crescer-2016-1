72b39d3305eeb481716113af6d1d29dfec3e604f
alycioneto <alycio@gmail.com>
2016-05-03 03:37:16 -0300

    feat(list-js-basico) Finalizado

--- src/modulo-04-JavaScript/dia01/lista-js-basico.js
+++ src/modulo-04-JavaScript/dia01/lista-js-basico.js
@@ -1,13 +1,81 @@
-'use strict'
+
 var daisyGame = function(flores){
-    return flores%2 === 0 ? "Love Me" : "Love Me Not"
+    return flores%2 === 0 ? "Love Me" : "Love Me Not";
 }
 
 var maiorTexto = function(array){
-  for( i in array.lenght){
-    var cont = array[i].lenght
-    var maior = 0;
-    maior = maior>cont ? maior:cont;
+  var maiorPalavra='';
+  for( var i=0; i < array.length; i++ ){
+    var palavraAtual = array[i];
+    if (palavraAtual.length > maiorPalavra.length)
+      maiorPalavra = palavraAtual;
   }
-  return ;
+  return maiorPalavra;
 }
+
+var imprime = function(instrutores, fn){
+    if (typeof fn === 'function'){
+      for(var i = 0; i< instrutores.length; i++){
+        fn(instrutores[i]);
+      }
+    }
+}
+var imprimeInstrutores =  function(instrutor) {
+     console.log('olá querido instrutor: ' + instrutor)
+}
+//http://stackoverflow.com/questions/18802026/function-calling-in-javascript-with-double-brackets
+var somar = function(primeiroValor){
+    return function(segundoValor){
+      return primeiroValor+segundoValor;
+    }
+}
+var fiboSum = function(tamanho) {
+  var sequencia= [];
+  var  i = 0;
+  var  j = 1;
+  var soma = 0;
+  sequencia.push(i);
+  sequencia.push(j);
+  while (sequencia.length <= tamanho) {
+    sequencia.push(sequencia[j] + sequencia[i]);
+    soma += sequencia[j];
+    j++;
+    i++;
+  }
+  return soma + sequencia[j];
+};
+
+//http://www.w3schools.com/jsref/jsref_sort.asp
+var queroCafe = function(mascada, precos){
+    precos.sort(function(a, b){return a-b});
+    for (var i = precos.length -1; mascada < precos[i]; i--) {
+        precos.pop();
+    }
+    return precos;
+}
+
+var contarPorTipo = function(objeto, string){
+    var cont=0;
+    for(var i in objeto){
+      if (typeof objeto[i] === string ){
+        cont++;
+      }
+      else if (string == "null") {
+        objeto[i] == string
+      }
+      else if(string == "array")
+      objeto[i] instanceof Array ? cont++:cont;
+   }
+   return cont;
+}
+
+// No momento que dentro da function, a variavel gohan é novamente declarada com var, ela volta a ser undefined, perdendo valor que já sido informado;
+var gohan = 'gohan', goku = 'Goku';
+function revelarSaiyaman() {
+  console.log(gohan);
+  goku = 'Son Goku';
+  var gohan = 'Son ' + gohan;
+  return gohan;
+}
+console.log(revelarSaiyaman());
+console.log(goku);
