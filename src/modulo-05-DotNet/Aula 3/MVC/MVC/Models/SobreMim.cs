df737e7971de4b608114284f557234aadabf8afe
Alycio Bertazzo Neto <alycio@gmail.com>
2016-05-11 02:39:57 -0300

    feat(dia3) exercicios e desafio pronto.

---
+++ src/modulo-05-DotNet/Aula 3/MVC/MVC/Models/SobreMim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class SobreMim
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Cidade { get; set; }
+    }
+}
