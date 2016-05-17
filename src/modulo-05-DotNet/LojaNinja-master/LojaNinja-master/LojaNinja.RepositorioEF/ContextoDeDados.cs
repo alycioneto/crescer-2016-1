83d08a2a86117a31fafe0ddafdabb79ce489b6ce
Alycio Bertazzo Neto <alycio.neto@cwinet.local>
2016-05-17 17:40:37 -0300

    feat(entityFreamwork) Iniciado a instalação do freamwork

---
+++ src/modulo-05-DotNet/LojaNinja-master/LojaNinja-master/LojaNinja.RepositorioEF/ContextoDeDados.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LojaNinja.Dominio;
+
+namespace LojaNinja.RepositorioEF
+{
+    class ContextoDeDados : DbContext
+    {
+        public ContextoDeDados() : base("ConexaoLojaNinja")
+        {
+        }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/modulo-05-DotNet/LojaNinja-master/LojaNinja-master/LojaNinja.RepositorioEF/Migrations/201605171938218_Initial.cs
@@ -0,0 +1,28 @@
+namespace LojaNinja.RepositorioEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Usuario",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        Senha = c.String(),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Usuario");
+        }
+    }
+}
