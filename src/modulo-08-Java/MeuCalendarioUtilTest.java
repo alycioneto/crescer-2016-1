3255a68b505188eaa2b3f8ea8c375f6d7e920e1c
alycioneto <alycio@gmail.com>
2016-06-08 01:28:12 -0300

    feat(MeuCalendarioUtilTest) Teste do metodo 1

---
+++ src/modulo-08-Java/MeuCalendarioUtilTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crescer.aula1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Alycio
+ */
+public class MeuCalendarioUtilTest {
+    
+    public MeuCalendarioUtilTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of dayOfWeek method, of class MeuCalendarioUtil.
+     */
+    @Test
+    public void testDayOfWeek() {
+        System.out.println("dayOfWeek");
+        Date data= new Date(2016, 5 , 7);         
+        MeuCalendarioUtil instance = new MeuCalendarioUtil();
+        String expResult = "Quarta";
+        String result = instance.dayOfWeek(data);
+        assertEquals(expResult, result);
+    }
+    
+}
